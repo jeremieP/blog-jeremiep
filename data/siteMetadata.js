b21a6f33fc7fe2783bd991867dbf1e107709b81e
jeremieP <jeremie.pereira@gmail.com>
2023-08-29 22:09:31 +0200

    update data for analytics, and socials

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -16,26 +16,10 @@ const siteMetadata = {
   twitter: 'https://x.com/jeremieP',
   youtube: 'https://youtube.com/@jeremieP_',
   linkedin: 'https://www.linkedin.com/',
+  twitch: 'https://www.twitch.tv/jeremieP_',
+  discord: 'https://discord.gg/hsAjtxah99',
+  playstation: 'https://psnprofiles.com/jayjayFR',
   locale: 'fr-FR',
-  analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
-      // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-    },
-    // plausibleAnalytics: {
-    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    // },
-    // simpleAnalytics: {},
-    // posthogAnalytics: {
-    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
-  },
   comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
